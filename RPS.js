30171284dc3e4ab7ddffc6eb17d10f60cb04c576
Shiva04-Dev <ganeshshiva772@gmail.com>
2025-01-11 14:24:59 +0200

    Created Gobal vari. to keep track of the player and computer's score. I also created a playRound function that executes the choices from the human and computer and provides us with a winner, after which the winner's score increases by 1.

--- RPS.js
+++ RPS.js
@@ -2,26 +2,65 @@ function getComputerChoice() {
     let comp = Math.floor(Math.random() * 3) + 1
 
     if (comp == 1){
-        console.log("ROCK")
+        return "ROCK"
     } else if (comp == 2) {
-        console.log("SCISSORS") 
+        return "SCISSORS" 
     } else {
-        console.log("PAPER")
+        return "PAPER"
     }
 }
-console.log(getComputerChoice())
 
 function getHumanChoice() {
-    let hum = (prompt("Do you choose to throw Rock, Paper or Scissors?")).charAt(0).toUpperCase()
+    let hum = (prompt("Do you choose to throw Rock, Paper or Scissors?")).toLowerCase()
 
-    if(hum == "R") {
-        console.log("ROCK")
-    } else if (hum == "P") {
-        console.log("PAPER")
-    } else if (hum == "S") {
-        console.log("SCISSORS")
+    if(hum == "rock") {
+        return "ROCK"
+    } else if (hum == "paper") {
+        return "PAPER"
+    } else if (hum == "scissors") {
+        return "SCISSORS"
     } else {
-        console.log("Invalid Choice")
+        return "Invalid Choice"
     }
 }
-console.log(getHumanChoice())
+
+let humanScore = 0
+let computerScore = 0
+
+function playRound(computerChoice, humanChoice) {
+
+    if ( humanChoice == "ROCK" && computerChoice == "PAPER") {
+        computerScore++
+        return "Paper Beats Rock, YOU LOSE!"
+    } else if (humanChoice == "PAPER" && computerChoice == "SCISSORS") {
+        computerScore++
+        return "Scissors Beats Paper, YOU LOSE!"
+    } else if (humanChoice == "SCISSORS" && computerChoice == "ROCK") {
+        computerScore++
+        return "Rock Beats Scissors, YOU LOSE!"
+    } else if (humanChoice == "ROCK" && computerChoice == "SCISSORS") {
+        humanScore++
+        return "Rock Beats Scissors, YOU WIN!!!"
+    } else if (humanChoice == "PAPER" && computerChoice == "ROCK") {
+        humanScore++
+        return "Paper Beats Rock, YOU WIN!!!"
+    } else if (humanChoice == "SCISSORS" && computerChoice == "PAPER") {
+        humanScore++
+        return "Scissors Beats Paper, YOU WIN!!!"
+    } else if(humanChoice == computerChoice){
+        return "--TIE--"
+    } else {
+        return "Invalid Input"
+    }
+
+}
+
+const computer = getComputerChoice()
+const human = getHumanChoice()
+
+console.log("Computer's Choice: " + computer)
+console.log("Your Choice: " + human)
+
+console.log(playRound(computer, human))
+console.log("Your Score: " + humanScore)
+console.log("Computer Score: " + computerScore)
